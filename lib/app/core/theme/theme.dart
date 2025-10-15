dc533e4842332de79207de224149a3282af133a6
rafiikkodev <rafiikkodev@gmail.com>
2025-10-15 13:33:48 +0700

    update theme

---
+++ lib/app/core/theme/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// primary color
+Color darkColor = const Color(0xff1f1d2b);
+Color softColor = const Color(0xff252836);
+Color darkBlueAccent = const Color(0xff12cdd9);
+
+// secondary color
+Color greenColor = const Color(0xff22b07d);
+Color orangeColor = const Color(0xffff8700);
+Color redColor = const Color(0xfffb4141);
+
+// text color
+Color blackColor = const Color(0xff171725);
+Color darkGreyColor = const Color(0xff696974);
+Color greyColor = const Color(0xff92929d);
+Color whiteGreyColor = const Color(0xffebebef);
+Color whiteColor = const Color(0xffffffff);
+Color lineDarkColor = const Color(0xffeaeaea);
+
+Color lightBackgoundColor = const Color(0xffF6F8FB);
+Color darkBackgroundColor = const Color(0xff020518);
+
+// inisialisasi warna text
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
+TextStyle darkBlueTextStyle = GoogleFonts.poppins(color: darkBlueAccent);
+TextStyle redTextStyle = GoogleFonts.poppins(color: redColor);
+
+// inisialisasi ketebalan text
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
