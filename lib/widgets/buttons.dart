38e6b7bf00aaa07210a18b9ba01852f2f219dc85
rafiikkodev <rafiikkodev@gmail.com>
2025-10-15 20:15:28 +0700

    progress button 2

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -179,10 +179,12 @@ class DefaultExtraLargeLabelButton extends StatelessWidget {
       height: height,
       child: OutlinedButton(
         style: OutlinedButton.styleFrom(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
           side: BorderSide(
             color: darkBlueAccent,
-            width: 2,
+            width: 1.5,
             style: BorderStyle.solid,
           ),
         ),
@@ -191,6 +193,317 @@ class DefaultExtraLargeLabelButton extends StatelessWidget {
           title,
           style: darkBlueTextStyle.copyWith(fontSize: 16, fontWeight: medium),
         ),
+      ),
+    );
+  }
+}
+
+class DefaultLargeLabelButton extends StatelessWidget {
+  final String title;
+  final double height;
+  final double width;
+  final VoidCallback? onPressed;
+
+  const DefaultLargeLabelButton({
+    super.key,
+    required this.title,
+    // this.width = double.infinity,
+    required this.width,
+    this.height = 52,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+          side: BorderSide(
+            color: darkBlueAccent,
+            width: 1.5,
+            style: BorderStyle.solid,
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          title,
+          style: darkBlueTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+        ),
+      ),
+    );
+  }
+}
+
+class DefaultMediumLabelButton extends StatelessWidget {
+  final String title;
+  final double height;
+  final double width;
+  final VoidCallback? onPressed;
+
+  const DefaultMediumLabelButton({
+    super.key,
+    required this.title,
+    // this.width = double.infinity,
+    required this.width,
+    this.height = 44,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+          side: BorderSide(
+            color: darkBlueAccent,
+            width: 1.5,
+            style: BorderStyle.solid,
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          title,
+          style: darkBlueTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+        ),
+      ),
+    );
+  }
+}
+
+class DefaultSmallLabelButton extends StatelessWidget {
+  final String title;
+  final double height;
+  final double width;
+  final VoidCallback? onPressed;
+
+  const DefaultSmallLabelButton({
+    super.key,
+    required this.title,
+    // this.width = double.infinity,
+    required this.width,
+    this.height = 36,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+          side: BorderSide(
+            color: darkBlueAccent,
+            width: 1.5,
+            style: BorderStyle.solid,
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          title,
+          style: darkBlueTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+        ),
+      ),
+    );
+  }
+}
+
+class DefaultExtraSmallLabelButton extends StatelessWidget {
+  final String title;
+  final double height;
+  final double width;
+  final VoidCallback? onPressed;
+
+  const DefaultExtraSmallLabelButton({
+    super.key,
+    required this.title,
+    // this.width = double.infinity,
+    required this.width,
+    this.height = 28,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+          side: BorderSide(
+            color: darkBlueAccent,
+            width: 1.5,
+            style: BorderStyle.solid,
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          title,
+          style: darkBlueTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+        ),
+      ),
+    );
+  }
+}
+
+// BUTTON TANPA BACKGROUND
+
+class TextExtraLargeLabelButton extends StatelessWidget {
+  final String title;
+  final double height;
+  final double width;
+  final VoidCallback? onPressed;
+
+  const TextExtraLargeLabelButton({
+    super.key,
+    required this.title,
+    // this.width = double.infinity,
+    required this.width,
+    this.height = 56,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        child: Text(title, style: greyTextStyle.copyWith(fontSize: 16)),
+      ),
+    );
+  }
+}
+
+class TextLargeLabelButton extends StatelessWidget {
+  final String title;
+  final double height;
+  final double width;
+  final VoidCallback? onPressed;
+
+  const TextLargeLabelButton({
+    super.key,
+    required this.title,
+    // this.width = double.infinity,
+    required this.width,
+    this.height = 52,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        child: Text(title, style: greyTextStyle.copyWith(fontSize: 16)),
+      ),
+    );
+  }
+}
+
+class TextMediumLabelButton extends StatelessWidget {
+  final String title;
+  final double height;
+  final double width;
+  final VoidCallback? onPressed;
+
+  const TextMediumLabelButton({
+    super.key,
+    required this.title,
+    // this.width = double.infinity,
+    required this.width,
+    this.height = 44,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        child: Text(title, style: greyTextStyle.copyWith(fontSize: 16)),
+      ),
+    );
+  }
+}
+
+class TextSmallLabelButton extends StatelessWidget {
+  final String title;
+  final double height;
+  final double width;
+  final VoidCallback? onPressed;
+
+  const TextSmallLabelButton({
+    super.key,
+    required this.title,
+    // this.width = double.infinity,
+    required this.width,
+    this.height = 36,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        child: Text(title, style: greyTextStyle.copyWith(fontSize: 16)),
+      ),
+    );
+  }
+}
+
+class TextExtraSmallLabelButton extends StatelessWidget {
+  final String title;
+  final double height;
+  final double width;
+  final VoidCallback? onPressed;
+
+  const TextExtraSmallLabelButton({
+    super.key,
+    required this.title,
+    // this.width = double.infinity,
+    required this.width,
+    this.height = 28,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        child: Text(title, style: greyTextStyle.copyWith(fontSize: 16)),
       ),
     );
   }
